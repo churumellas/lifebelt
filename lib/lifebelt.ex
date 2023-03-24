5f03b75b8998a043bb013276409288aa95dc7682
Almir Neto <almir.as.neto03@gmail.com>
2023-03-24 13:16:44 -0300

    finish naive lifeline clone

--- lib/lifebelt.ex
+++ lib/lifebelt.ex
@@ -38,4 +38,91 @@ defmodule Lifebelt do
       option -> {:unknown, option, State}
     end)
   end
+
+  @impl GenServer
+  def init(opts) do
+    Validation.validate!(opts, &validate/1)
+
+    state =
+      State
+      |> struct!(opts)
+      |> schedule_rescue()
+
+    :telemetry.execute([:oban, :plugin, :init], %{}, %{conf: state.conf, plugin: __MODULE__})
+
+    {:ok, state}
+  end
+
+  @impl GenServer
+  def terminate(_reason, %State{timer: timer}) do
+    if is_reference(timer), do: Process.cancel_timer(timer)
+
+    :ok
+  end
+
+  @impl GenServer
+  def handle_info(:rescue, %State{} = state) do
+    meta = %{conf: state.conf, plugin: __MODULE__}
+
+    :telemetry.span([:oban, :plugin], meta, fn ->
+      case check_leadership_and_rescue_jobs(state) do
+        {:ok, extra} when is_map(extra) ->
+          {:ok, Map.merge(meta, extra)}
+
+        error ->
+          {:error, Map.put(meta, :error, error)}
+      end
+    end)
+
+    {:noreply, schedule_rescue(state)}
+  end
+
+
+  # Scheduling
+
+  defp schedule_rescue(state) do
+    timer = Process.send_after(self(), :rescue, state.interval)
+
+    %{state | timer: timer}
+  end
+  # Rescuing
+
+  defp check_leadership_and_rescue_jobs(state) do
+    if Peer.leader?(state.conf) do
+      Repo.transaction(state.conf, fn ->
+        time = DateTime.add(DateTime.utc_now(), -state.rescue_after, :millisecond)
+        base = where(Job, [j], j.state == "executing" and j.attempted_at < ^time)
+
+        {rescued_count, rescued} = transition_available(base, state)
+        {discard_count, discard} = transition_discarded(base, state)
+
+        %{
+          discarded_count: discard_count,
+          discarded_jobs: discard,
+          rescued_count: rescued_count,
+          rescued_jobs: rescued
+        }
+      end)
+    else
+      {:ok, %{}}
+    end
+  end
+
+  defp transition_available(base, state) do
+    query =
+      base
+      |> where([j], j.attempt < j.max_attempts)
+      |> select([j], map(j, [:id, :queue, :state]))
+
+    Repo.update_all(state.conf, query, set: [state: "available"])
+  end
+
+  defp transition_discarded(base, state) do
+    query =
+      base
+      |> where([j], j.attempt >= j.max_attempts)
+      |> select([j], map(j, [:id, :queue, :state]))
+
+    Repo.update_all(state.conf, query, set: [state: "discarded", discarded_at: DateTime.utc_now()])
+  end
 end
